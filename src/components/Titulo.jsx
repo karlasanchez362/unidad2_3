744dd14fd523a360b52296cea917c459994e620f
karlasanchez362 <karlagsanchez362@gmail.com>
2025-09-06 18:01:10 -0300

    Componente Titulo agregado

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Titulo() {
+  return (
+    <div style={{textAlign: 'center' , backgroundColor: '#f8b097', padding: '20px', borderRadius: '10px', margin: '20px'}}>
+      <h1>Mi perfil</h1> 
+      <p>
+        Hola, mi nombre es Karla Sanchez, tengo  24 años y soy estudiante de la tecnicatura 
+        de Analisis de Sistemas del Instituto Superior del Milagro en la ciudad de Salta, Argentina.
+      </p>
+    </div>
+  )
+}
