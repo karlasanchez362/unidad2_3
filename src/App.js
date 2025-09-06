0937735ba4ab6d280692ea93287c9333c4576804
karlasanchez362 <karlagsanchez362@gmail.com>
2025-09-06 18:36:55 -0300

    Modificaciones en el componente Tarjeta

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Titulo from './components/Titulo';
 import TarjetaDePerfil from './components/TarjetaDePerfil'; 
 
-function App() {
-  const usuario = {
+const usuario = {
     nombre: 'Karla Sanchez',
     edad: 24,
     profesion: 'Estudiante de Analisis de Sistemas',
     ciudad: 'Salta, Argentina'
   };
+function App() {
+  
   return (
     <div className="App">
       <Titulo />
--- src/components/TarjetaDePerfil.jsx
+++ src/components/TarjetaDePerfil.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function TarjetaDePerfil({ usuario }) {
   return (
-    <div style={{ border: '1px solid #ccc', borderRadius: '10px', padding: '10px', margin: '10px' , backgroundColor: '#77acb7'}}>
+    <div style={{ border: '1px solid #ccc', borderRadius: '10px', padding: '20px', margin: '20px auto',
+    boxShadow: '0 4px 10px rgba(0,0,0,0.1)', backgroundColor: 'linear-gradient(135deg, #f9f9f9, #eaeaea)',
+    maxWidth: '300px' , textAlign: 'center'}}>
       <h2>{usuario.nombre}</h2>
       <p>Edad: {usuario.edad}</p>
       <p>Profesión: {usuario.profesion}</p>
